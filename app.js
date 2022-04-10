262b754b361d55dc1ab80d996615f83a99f9c7a7
Jared Schwalbe <Jared.Schwalbe@ibm.com>
2022-04-10 12:07:53 -0500

    feat: add interval to wait for ads container to appear

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
+// immediately hide the ads from the user
+const style = document.createElement('style');
+style.textContent = '.app > div:not(:first-child) { visibility: hidden; }';
+document.head.appendChild(style);
+
+// push the ads off the page by expanding window.innerWidth to exceed the actual window width
 function addResizeCanvasEvent() {
   document.addEventListener('resizecanvas', (e) => {
     window.innerWidth = document.documentElement.clientWidth + e.detail.adsWidth;
   });
 }
 
+// inject our custom event listener into the "main world"
+document.documentElement.setAttribute('onreset', `(${addResizeCanvasEvent})()`);
+document.documentElement.dispatchEvent(new CustomEvent('reset'));
+document.documentElement.removeAttribute('onreset');
+
 function resizeWindow(e, adsWidth) {
-  if (e.skip) {
-    return;
+  if (!e.skip) {
+    // expand window.innerWidth through our custom event
+    document.dispatchEvent(new CustomEvent('resizecanvas', { detail: { adsWidth } }));
+
+    // trigger another resize event to update any listeners with the new window.innerWidth
+    const resizeEvent = new Event('resize');
+    resizeEvent.skip = true;
+    window.dispatchEvent(resizeEvent);
   }
-
-  // run our custom event to modify the window.innerWidth
-  document.dispatchEvent(new CustomEvent('resizecanvas', {
-    detail: { adsWidth },
-  }))
-
-  // manually trigger the resize event to update listeners with the new window.innerWidth
-  const resizeEvent = new Event('resize');
-  resizeEvent.skip = true;
-  window.dispatchEvent(resizeEvent);
 };
 
-setTimeout(() => {
-  // set up custom resize event to interact with the "main world"
-  document.documentElement.setAttribute('onreset', `(${addResizeCanvasEvent})()`);
-  document.documentElement.dispatchEvent(new CustomEvent('reset'));
-  document.documentElement.removeAttribute('onreset');
-
-  // hide the ads from the user
-  const style = document.createElement('style');
-  style.textContent = '.app > div:not(:first-child) { visibility: hidden; }';
-  document.head.appendChild(style);
-
-  // calculate the width of the ads
+// wait for the ads container to load into the DOM
+const initialResize = setInterval(() => {
   const app = document.querySelector('.app');
   const content = app && app.childElementCount ? app.children[0] : {};
   const adsWidth = window.innerWidth - content.offsetWidth;
 
-  // slight delay helps with exceeding call stack size
-  let resizeTimeout;
-  window.onresize = (e) => {
-    clearTimeout(resizeTimeout);
-    resizeTimeout = setTimeout(() => resizeWindow(e, adsWidth), 100);
-  };
+  if (adsWidth) {
+    clearInterval(initialResize);
 
-  // begin by triggering a resize event
-  window.dispatchEvent(new CustomEvent('resize'));
-}, 100);
+    // a slight delay helps with exceeding call stack size
+    let resizeTimeout;
+    window.onresize = (e) => {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(() => resizeWindow(e, adsWidth), 100);
+    };
+
+    // begin by triggering a resize event
+    window.dispatchEvent(new CustomEvent('resize'));
+  }
+}, 200);
